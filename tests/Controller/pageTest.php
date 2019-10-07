723810d8edc38615b523e48ff94856160ffb36a5
alihasana <hasana.ali@gmail.com>
2019-10-07 16:50:39 +0200

    add comment

--- tests/Controller/pageTest.php
+++ tests/Controller/pageTest.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class pageTest extends WebTestCase
 {
     public function testRun() {
+        // to run ten for calculator
         $client = static::createClient();
         $client->request('GET', '/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
